cf1e4018a07d24c9b60d8166a3b916af21e9ccbf
yoosion <rsy5487@naver.com>
2021-09-20 14:09:54 +0900

    JS 위치 변경

---
+++ src/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#fullpage').fullpage();
+});
+// 모든 a 태그 속성을 target = "_blank"로 적용
+var a = document.getElementsByTagName('a');
+
+for (var i = 0; i < a.length; i++) {
+
+    a[i].target = "_blank";
+
+}
