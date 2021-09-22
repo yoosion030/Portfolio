9e7255e6c667ffa980f87f4c01f5832966ce70b4
yoosion <rsy5487@naver.com>
2021-09-22 23:09:39 +0900

    Change ClassName

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,32 +1,32 @@
-const white = document.querySelectorAll('.white-selection');
-const black = document.querySelectorAll('.black-selection');
+const white = document.querySelectorAll('.white');
+const black = document.querySelectorAll('.black');
 const darkMode = document.querySelector('#dark-mode');
 
 darkMode.addEventListener('click', e => {
     if (e.target.checked) {
         for (let i = 0; i < white.length; i++) {
-            white[i].classList.remove('white-selection');
-            white[i].classList.add('black-selection');
+            white[i].classList.remove('white');
+            white[i].classList.add('black');
             for (let i = 0; i < a.length; i++) {
                 a[i].style = "color : #fafbf9";
             }
         }
         for (let i = 0; i < black.length; i++) {
-            black[i].classList.remove('black-selection');
-            black[i].classList.add('white-selection');
+            black[i].classList.remove('black');
+            black[i].classList.add('white');
         }
     } 
     else {
         for (let i = 0; i < white.length; i++) {
-            white[i].classList.remove('black-selection');
-            white[i].classList.add('white-selection');
+            white[i].classList.remove('black');
+            white[i].classList.add('white');
             for (let i = 0; i < a.length; i++) {
                 a[i].style = "color : #242424";
             }
         }
         for (let i = 0; i < black.length; i++) {
-            black[i].classList.remove('white-selection');
-            black[i].classList.add('black-selection');
+            black[i].classList.remove('white');
+            black[i].classList.add('black');
         }
     }
 });
