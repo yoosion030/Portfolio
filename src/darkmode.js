31a1865b102aa816df91b6dfcc182ca9f0259d23
yoosion <rsy5487@naver.com>
2021-10-27 08:28:53 +0900

    Code Refactoring

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,6 +1,7 @@
 const white = document.querySelectorAll('.white');
 const black = document.querySelectorAll('.black');
 const darkMode = document.querySelector('#dark-mode');
+const contactA = document.querySelector('.e-mail');
 
 darkMode.addEventListener('click', e => {
     if (e.target.checked) {
@@ -8,7 +9,8 @@ darkMode.addEventListener('click', e => {
             white[i].classList.remove('white');
             white[i].classList.add('black');
             for (let i = 0; i < a.length; i++) {
-                a[i].style = "color : #fafbf9";
+                a[i].style = "color: var(--white-color);";
+                contactA.style = "color : var(--black-color)";
             }
         }
         for (let i = 0; i < black.length; i++) {
@@ -21,7 +23,8 @@ darkMode.addEventListener('click', e => {
             white[i].classList.remove('black');
             white[i].classList.add('white');
             for (let i = 0; i < a.length; i++) {
-                a[i].style = "color : #242424";
+                a[i].style = "color: var(--black-color);";
+                contactA.style = "color: var(--white-color);";
             }
         }
         for (let i = 0; i < black.length; i++) {
